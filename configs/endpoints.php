d1f41fb07375342620c92245919cf7d8f08bf2ae
Amir Salehi <iamamirsalehi@gmail.com>
2023-04-22 15:36:06 +0330

    TwoFaBasedAuth methods implemented

--- configs/endpoints.php
+++ configs/endpoints.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'base' => $GLOBALS['coreUrl'],
-    'app_url' => $GLOBALS['appUrl'],
+//    'base' => $GLOBALS['coreUrl'],
+//    'app_url' => $GLOBALS['appUrl'],
+    'base' => $GLOBALS['coreUrl'] ?? 'http://127.0.0.1:8080/api/',
+    'app_url' => $GLOBALS['appUrl'] ?? 'http://localhost:2000',
 ];
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 include_once __DIR__ . "/vendor/autoload.php";
+
+
+print_r(\Ls\ClientAssistant\Utilities\Modules\TwoFaBasedAuth::login('09120009999'));
