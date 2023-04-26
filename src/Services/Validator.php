3824efe5b9194d482bdd3dbcd6769ba9f30f8878
Amir Salehi <iamamirsalehi@gmail.com>
2023-04-26 13:49:01 +0330

    toPersian implemented

--- src/Services/Validator.php
+++ src/Services/Validator.php
@@ -2,6 +2,7 @@
 
 namespace Ls\ClientAssistant\Services;
 
+use Ls\ClientAssistant\Helpers\Str;
 use Respect\Validation\Validator as v;
 
 class Validator
@@ -13,8 +14,8 @@ class Validator
     {
         $validator = new self();
 
-        $isEmail = v::stringVal()->email()->validate($data['input']);
-        $isMobile = v::numericVal()->regex('/((\+98|0)?9\d{9})|((\+)?\d{10,12})/')->validate($data['input']);
+        $isEmail = v::stringVal()->email()->validate(Str::toPersian($data['input']));
+        $isMobile = v::numericVal()->regex('/((\+98|0)?9\d{9})|((\+)?\d{10,12})/')->validate(Str::toPersian($data['input']));
 
         if (!$isEmail and !$isMobile) {
             $validator->ok = false;
@@ -24,7 +25,7 @@ class Validator
         }
 
         if (isset($data['password'])) {
-            $notBlankPassword = v::notBlank()->min(3)->validate($data['password']);
+            $notBlankPassword = v::notBlank()->min(3)->validate(Str::toPersian($data['input']));
             if (!$notBlankPassword) {
                 $validator->ok = false;
                 $validator->errors[] = 'رمز عبور نباید خالی باشد';
@@ -42,9 +43,9 @@ class Validator
     {
         $validator = new self();
 
-        $isEmail = v::stringVal()->email()->validate($data['input']);
-        $isMobile = v::numericVal()->regex('/((\+98|0)?9\d{9})|((\+)?\d{10,12})/')->validate($data['input']);
-        $isValidOtp = v::numericVal()->validate($data['otp']);
+        $isEmail = v::stringVal()->email()->validate(Str::toPersian($data['input']));
+        $isMobile = v::numericVal()->regex('/((\+98|0)?9\d{9})|((\+)?\d{10,12})/')->validate(Str::toPersian($data['input']));
+        $isValidOtp = v::numericVal()->validate(Str::toPersian($data['input']));
 
         if (!$isEmail and !$isMobile) {
             $validator->ok = false;
