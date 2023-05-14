6546ad3ce73d7fd36085f07e94bc6d1bebf07255
Amir Salehi <iamamirsalehi@gmail.com>
2023-05-15 00:52:09 +0330

    signal method implemented in Enrollment utility

--- src/Utilities/Modules/Enrollment.php
+++ src/Utilities/Modules/Enrollment.php
@@ -11,14 +11,14 @@ class Enrollment extends ModuleUtility
 {
     public static function get(string $id, array $with = []): Collection
     {
-        return GuzzleClient::get('v1/enrollment/' . $id, [
+        return GuzzleClient::get('v1/lms/enrollment/' . $id, [
             'with' => json_encode($with),
         ]);
     }
 
     public static function list(array $with = [], array $keyValues = [], int $perPage = 20, $orderBy = OrderByEnum::LATEST): Collection
     {
-        return GuzzleClient::get('v1/enrollment', [
+        return GuzzleClient::get('v1/lms/enrollment', [
             'with' => json_encode($with),
             'filter' => json_encode($keyValues),
             'order_by' => $orderBy,
@@ -28,11 +28,24 @@ class Enrollment extends ModuleUtility
 
     public static function search(string $keyword, array $columns = [], array $with = [], int $perPage = 20): Collection
     {
-        return GuzzleClient::get('v1/enrollment', [
+        return GuzzleClient::get('v1/lms/enrollment', [
             's' => $keyword,
             'with' => json_encode($with),
             'columns' => json_encode($columns),
             'per_page' => $perPage,
         ]);
+    }
+
+    public static function signal(int $enrollmentId, int $productItem, string $userToken, string $type): Collection
+    {
+        if (!in_array($type, ['visited', 'completed', 'played'])) {
+            throw new \InvalidArgumentException('Type must be in [visited, completed, played]');
+        }
+
+        $response = GuzzleClient::put(('v1/lms/enrollment/' . $enrollmentId . '/signal/' . $productItem), [
+            'signal' => $type,
+        ], ['Authorization' => 'Bearer ' . $userToken]);
+
+        return GuzzleClient::parseData($response);
     }
 }
