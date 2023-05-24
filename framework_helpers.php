7eac871cef358fe206c97662cf65a75c01447839
Amir Salehi <iamamirsalehi@gmail.com>
2023-05-24 19:41:54 +0330

    framework_helpers.php updated

--- framework_helpers.php
+++ framework_helpers.php
@@ -1,31 +1,41 @@
 <?php
 
-function site_url(string $uri): string
-{
-    return ($GLOBALS['appUrl'] ?? '')  . $uri;
+if (!function_exists('site_url')) {
+    function site_url(string $uri): string
+    {
+        return ($GLOBALS['appUrl'] ?? '') . $uri;
+    }
 }
 
-function asset_url(string $path): string
-{
-    return ($GLOBALS['appUrl'] ?? '') . '/assets/' . $path;
+if (!function_exists('asset_url')) {
+    function asset_url(string $path): string
+    {
+        return ($GLOBALS['appUrl'] ?? '') . '/assets/' . $path;
+    }
 }
 
-function storage_url(string $path = null): string
-{
-    return ($GLOBALS['storageUrl'] ?? '') . '/' . $path;
+if (!function_exists('storage_url')) {
+    function storage_url(string $path = null): string
+    {
+        return ($GLOBALS['storageUrl'] ?? '') . '/' . $path;
+    }
 }
 
-function core_url(string $path = null): string
-{
-    return rtrim($GLOBALS['coreUrl'] ?? '', '/api/') . 'framework_helpers.php/' . $path;
+if (!function_exists('core_url')) {
+    function core_url(string $path = null)
+    {
+        return rtrim($_ENV['CORE_URL'] ?? '', '/api/') . '/' . $path;
+    }
 }
 
-function redirect($url)
-{
-    header("Location: $url");
-    exit();
-}
 
+if (!function_exists('redirect')) {
+    function redirect($url)
+    {
+        header("Location: $url");
+        exit();
+    }
+}
 
 /*function view(string $view, array $vars = [])
 {
