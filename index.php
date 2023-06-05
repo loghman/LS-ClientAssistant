90124d435cdfc7a029decdd09d838c910b31b235
Amir Salehi <iamamirsalehi@gmail.com>
2023-06-05 13:46:19 +0330

    updated

--- index.php
+++ index.php
@@ -1,20 +1,3 @@
 <?php
 
 include_once __DIR__ . "/vendor/autoload.php";
-
-$userToken = '4|JT0VHLX5Rmkaaa3rpatKHD3T7xwTjzxPeu2LGHCT';
-
-//print_r(\Ls\ClientAssistant\Utilities\Modules\Enrollment::logs(4, $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\ChapterEnrollment::forUser($userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\LMSProduct::faculty(2));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\LMSProduct::demo(2));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\OKBTopic::get(9));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\OKBTopic::list());
-//print_r(\Ls\ClientAssistant\Utilities\Modules\QC::addReview(['item_id' => 1, 'product_id' => 2, 'rate' => 2,], $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportCommunity::stats());
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportTopic::get(1));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\LMSProduct::createTopic(['item_id' => 3, 'title' => 'asjfahfsshfsjdf', 'content' => 'alskhjajlkdhfjlsjahjhfgsadhfgjkedshgjksfdhdhfgnefdsg'], $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportReply::reply(['topic_id' => 9, 'content' => 'salamaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'], $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportReply::update(['topic_id' => 9, 'reply_id' => 31, 'content' => 'salamaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'], $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportReply::like(9, 31, $userToken));
-//print_r(\Ls\ClientAssistant\Utilities\Modules\SupportReply::delete(9, 31, $userToken));
