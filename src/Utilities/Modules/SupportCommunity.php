a5391270b26f90623041e85db7930d70cf5df908
Amir Salehi <iamamirsalehi@gmail.com>
2023-06-06 08:17:20 +0330

    list of support community implemented

--- src/Utilities/Modules/SupportCommunity.php
+++ src/Utilities/Modules/SupportCommunity.php
@@ -3,11 +3,27 @@
 namespace Ls\ClientAssistant\Utilities\Modules;
 
 use Illuminate\Support\Collection;
+use Ls\ClientAssistant\Core\Contracts\ModuleUtility;
+use Ls\ClientAssistant\Core\Enums\OrderByEnum;
 use Ls\ClientAssistant\Core\GuzzleClient;
 use Ls\ClientAssistant\Helpers\Response;
 
 class SupportCommunity
 {
+    public static function list(array $with = [], array $keyValues = [], int $perPage = 20, $orderBy = OrderByEnum::LATEST): Collection
+    {
+        try {
+            return GuzzleClient::get('v1/support/community', [
+                'with' => json_encode($with),
+                'filter' => json_encode($keyValues),
+                'order_by' => $orderBy,
+                'per_page' => $perPage,
+            ]);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+
     public static function stats(): Collection
     {
         try {
