d9d50bdfc2f8605fb9d4dd1ef4f14f9249b27567
Amir Salehi <iamamirsalehi@gmail.com>
2023-06-07 17:18:55 +0330

    price tool updated

--- src/Utilities/Tools/Price.php
+++ src/Utilities/Tools/Price.php
@@ -6,7 +6,6 @@ class Price
 {
     public static function toPersianPrice($price, $noSpan = 0, $round = 0, $postfix = null): string
     {
-
         $currencyStr = is_null($postfix) ? "تومان" : "تومان$postfix";
         if ($round === 'hezar' || $round === 'thousand') {
             $price = round($price / 1000) * 1000;
@@ -17,11 +16,11 @@ class Price
         }
 
         if ($price < 1000) {
-            $str = Lang::persianNumbers($price) . ($noSpan ? " $currencyStr" : " <span class='currency mx-1'>$currencyStr</span>");
+            $str = Lang::persianNumbers($price) . " $currencyStr";
         } else if ($price < 1000000) {
-            $str = Lang::persianNumbers(round($price / 1000)) . ($noSpan ? " هزار $currencyStr" : " <span class='currency mx-1'>هزار $currencyStr</span>");
+            $str = Lang::persianNumbers(round($price / 1000)) . " هزار $currencyStr";
         } else {
-            $str = Lang::persianNumbers(round($price / 1000000, 3)) . ($noSpan ? " میلیون $currencyStr" : " <span class='currency mx-1'>میلیون $currencyStr</span>");
+            $str = Lang::persianNumbers(round($price / 1000000, 3)) . " میلیون $currencyStr";
         }
 
         return $str;
