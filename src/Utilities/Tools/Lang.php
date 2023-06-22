64ed43106714d521348674884ccbd92c119ae46a
Amir Salehi <iamamirsalehi@gmail.com>
2023-06-22 12:29:36 +0330

    convertSecondsToPersianInLineTime implemented

--- src/Utilities/Tools/Lang.php
+++ src/Utilities/Tools/Lang.php
@@ -4,14 +4,14 @@ namespace Ls\ClientAssistant\Utilities\Tools;
 
 class Lang
 {
-    public static function persianNumbers($input): array|string
+    public static function persianNumbers($input)
     {
         $faNum = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
         $enNum = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
         return str_replace($enNum, $faNum, (string)$input);
     }
 
-    public static function latinNumbers($input): array|string
+    public static function latinNumbers($input)
     {
         $faNum = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹');
         $enNum = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
