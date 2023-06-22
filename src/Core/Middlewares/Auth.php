3f723711707409f2037f7aafdf5c81ec957184ed
Ali khosravi <alikhosravi175@gmail.com>
2023-06-22 08:50:01 +0330

    checked mobile and email is verified

--- src/Core/Middlewares/Auth.php
+++ src/Core/Middlewares/Auth.php
@@ -5,14 +5,27 @@ namespace Ls\ClientAssistant\Core\Middlewares;
 use Ls\ClientAssistant\Utilities\Modules\User;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-class Auth implements \Psr\Http\Server\MiddlewareInterface
+class Auth implements MiddlewareInterface
 {
     public function process(Request $request, RequestHandler $handler): Response
     {
         if (!User::loggedIn()) {
             redirect(site_url('auth'));
+        }
+
+        if (in_array($request->getUri()->getPath(), ['/auth/logout', '/auth/verify', '/email/update', '/mobile/update'])) {
+            return $handler->handle($request);
+        }
+
+        $verificationFields = get_verification_fields();
+        if (
+            (in_array('email', $verificationFields) && ! User::emailVerified()) ||
+            (in_array('mobile', $verificationFields) && ! User::mobileVerified())
+        ) {
+            redirect(site_url('auth/verify'));
         }
 
         return $handler->handle($request);
--- src/Core/Middlewares/Guess.php
+++ src/Core/Middlewares/Guess.php
@@ -5,9 +5,10 @@ namespace Ls\ClientAssistant\Core\Middlewares;
 use Ls\ClientAssistant\Utilities\Modules\User;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class Guess implements \Psr\Http\Server\MiddlewareInterface
+class Guess implements MiddlewareInterface
 {
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
