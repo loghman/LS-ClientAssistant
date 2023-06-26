c290abdc819c19e1755e3a48f1151fe100f584aa
Ali khosravi <alikhosravi175@gmail.com>
2023-06-26 05:48:08 +0330

    added cart endpoints

--- src/Utilities/Modules/Cart.php
+++ src/Utilities/Modules/Cart.php
@@ -2,56 +2,49 @@
 
 namespace Ls\ClientAssistant\Utilities\Modules;
 
+use Exception;
 use GuzzleHttp\Exception\ClientException;
 use Illuminate\Support\Collection;
-use Ls\ClientAssistant\Core\Contracts\ModuleUtility;
-use Ls\ClientAssistant\Core\Enums\OrderByEnum;
 use Ls\ClientAssistant\Core\GuzzleClient;
 use Ls\ClientAssistant\Helpers\Response;
 
-class Cart extends ModuleUtility
+class Cart
 {
-    public static function get(string $id, array $with = []): Collection
+    public static function screen($userToken): Collection
     {
         try {
-            return GuzzleClient::get('v1/cart/' . $id, [
-                'with' => json_encode($with),
+            return GuzzleClient::get('v1/cart/screen', [], [
+                'Authorization' => 'Bearer '.$userToken,
             ]);
         } catch (ClientException $exception) {
             return Response::parseClientException($exception);
-        }catch (\Exception $exception){
+        } catch (Exception $exception) {
             return Response::parseException($exception);
         }
     }
 
-    public static function list(array $with = [], array $keyValues = [], int $perPage = 20, $orderBy = OrderByEnum::LATEST): Collection
+    public static function addItem($userToken)
     {
         try {
-            return GuzzleClient::get('v1/cart/', [
-                'with' => json_encode($with),
-                'filter' => json_encode($keyValues),
-                'order_by' => $orderBy,
-                'per_page' => $perPage,
+            return GuzzleClient::post('v1/cart/add', [], [
+                'Authorization' => 'Bearer '.$userToken,
             ]);
-        }  catch (ClientException $exception) {
+        } catch (ClientException $exception) {
             return Response::parseClientException($exception);
-        }catch (\Exception $exception){
+        } catch (Exception $exception) {
             return Response::parseException($exception);
         }
     }
 
-    public static function search(string $keyword, array $columns = [], array $with = [], int $perPage = 20): Collection
+    public static function deleteItem($userToken, $itemId)
     {
         try {
-            return GuzzleClient::get('v1/cart/', [
-                's' => $keyword,
-                'with' => json_encode($with),
-                'columns' => json_encode($columns),
-                'per_page' => $perPage,
+            return GuzzleClient::delete("v1/cart/delete/$itemId", [], [
+                'Authorization' => 'Bearer '.$userToken,
             ]);
-        }  catch (ClientException $exception) {
+        } catch (ClientException $exception) {
             return Response::parseClientException($exception);
-        }catch (\Exception $exception){
+        } catch (Exception $exception) {
             return Response::parseException($exception);
         }
     }
