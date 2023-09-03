75af959113f4764219183aba2849f3e99508559e
Amir Salehi <iamamirsalehi@gmail.com>
2023-09-03 10:31:25 +0330

    paginator bug fixed

--- sdk/Utilities/Tools/Paginator.php
+++ sdk/Utilities/Tools/Paginator.php
@@ -2,15 +2,13 @@
 
 namespace Ls\ClientAssistant\Utilities\Tools;
 
-use Ls\ClientAssistant\Helpers\Config;
-
 class Paginator
 {
     public static function setLink(array $paginatedData): array
     {
         if (isset($paginatedData['data']['first_page_url'])) {
             $currentLink = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-            $actualLink = str_replace('?' . $_SERVER['QUERY_STRING'], '', $currentLink);
+            $actualLink = str_replace('?' . ($_SERVER['QUERY_STRING'] ?? ''), '', $currentLink);
 
             $firstPageUrl = $actualLink . ($paginatedData['data']['first_page_url'] ? substr($paginatedData['data']['first_page_url'], strpos($paginatedData['data']['first_page_url'], '?')) : '');
             $lastPageUrl = $actualLink . ($paginatedData['data']['last_page_url'] ? substr($paginatedData['data']['last_page_url'], strpos($paginatedData['data']['last_page_url'], '?')) : '');
@@ -23,18 +21,20 @@ class Paginator
 
             foreach ($paginatedData['data']['links'] as $key => $link) {
                 if (!is_null($link['url'])) {
+                    $matches = [];
+                    $restOfTheUri = '';
                     if ($currentLink != $actualLink) {
                         $restOfTheUri = str_replace('?', '&', substr($currentLink, strpos($currentLink, '?')));
                         preg_match_all('/&page=\d+/', $restOfTheUri, $matches);
                     }
-                    $paginatedData['data']['links'][$key]['url'] = $actualLink . substr($link['url'], strpos($link['url'], '?')) . str_replace($matches[0], '', $restOfTheUri);
+                    $paginatedData['data']['links'][$key]['url'] = $actualLink . substr($link['url'], strpos($link['url'], '?')) . str_replace(($matches[0] ??''), '', $restOfTheUri);
                 }
             }
         }
 
         if (isset($paginatedData['first_page_url'])) {
             $currentLink = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-            $actualLink = str_replace('?' . $_SERVER['QUERY_STRING'], '', $currentLink);
+            $actualLink = str_replace(('?' . ($_SERVER['QUERY_STRING'] ?? '')), '', $currentLink);
 
             $firstPageUrl = $actualLink . ($paginatedData['first_page_url'] ? substr($paginatedData['first_page_url'], strpos($paginatedData['first_page_url'], '?')) : '');
             $lastPageUrl = $actualLink . ($paginatedData['last_page_url'] ? substr($paginatedData['last_page_url'], strpos($paginatedData['last_page_url'], '?')) : '');
@@ -47,12 +47,14 @@ class Paginator
 
             foreach ($paginatedData['links'] as $key => $link) {
                 if (!is_null($link['url'])) {
+                    $matches = [];
+                    $restOfTheUri = '';
                     if ($currentLink != $actualLink) {
                         $restOfTheUri = str_replace('?', '&', substr($currentLink, strpos($currentLink, '?')));
                         preg_match_all('/&page=\d+/', $restOfTheUri, $matches);
                     }
 
-                    $paginatedData['links'][$key]['url'] = $actualLink . substr($link['url'], strpos($link['url'], '?')) . str_replace($matches[0], '', $restOfTheUri);
+                    $paginatedData['links'][$key]['url'] = $actualLink . substr($link['url'], strpos($link['url'], '?')) . str_replace(($matches[0] ?? ''), '', $restOfTheUri);
                 }
             }
         }
