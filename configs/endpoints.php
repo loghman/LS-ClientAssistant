e085639b9f2eb0c4125bd4470de13308153608b8
Ali khosravi <alikhosravi175@gmail.com>
2023-09-17 06:58:41 +0330

    Added NoCaptcha

--- configs/endpoints.php
+++ configs/endpoints.php
@@ -8,6 +8,7 @@ return [
         'verification_fields', 'can_user_logged_in_with_password',
         'brand_name_fa', 'brand_name_en', 'site_title', 'site_description', 'social_links',
         'client_cache_driver', 'client_cache_request_setting',
+        '_env_google_recaptcha_v3_site_key', '_env_google_recaptcha_v3_secret_key', 'google_recaptcha_v3_status',
         'client_cache_request_cms', 'client_cache_request_lms', 'client_cache_request_hlms', 'client_cache_request_shop',
         'client_cache_revalidation_time', 'client_cache_active_for_logged_in_user',
         'client_static_cache_driver', 'client_static_cache_index_page','client_static_cache_blog_page', 'client_static_cache_single_blog',
