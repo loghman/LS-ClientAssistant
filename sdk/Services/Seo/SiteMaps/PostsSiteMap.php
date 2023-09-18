18bf83ab2edbc65e6a983059e7536f4daa0e2dd8
Mohammad Gazori <amohammadgazori38@gmail.com>
2023-09-18 14:01:16 +0330

    fix product sitemap

--- sdk/Services/Seo/SiteMaps/PostsSiteMap.php
+++ sdk/Services/Seo/SiteMaps/PostsSiteMap.php
@@ -18,7 +18,7 @@ class PostsSiteMap extends SiteMap
         foreach ($this->data as $item) {
             $item['changefreq'] = $item['changefreq'] ?? 'hourly';
             $item['priority'] = $item['priority'] ?? '0.8';
-//            $item['caption'] = html_entity_decode(sub_words($item['content'], 165), ENT_COMPAT, 'UTF-8');
+            $item['caption'] = html_entity_decode(sub_words($item['content'], 165), ENT_COMPAT, 'UTF-8');
             $item['loc'] = site_url('blog/' . $item['slug']);
             $item['lastmod'] = date('Y-m-d\TH:i:s+03:30', strtotime($item['updated_at']));
 
@@ -28,10 +28,13 @@ class PostsSiteMap extends SiteMap
             $items .= "<priority>{$item['priority']}</priority>" . PHP_EOL;
             $items .= "<lastmod>{$item['lastmod']}</lastmod>" . PHP_EOL;
 
+            $item['caption'] = strip_tags($item['caption']);
+
             if (!empty($item['thumbnail'])) {
                 $items .= "<image:image>" . PHP_EOL;
                 $items .= "<image:loc>{$item['thumbnail']}</image:loc>" . PHP_EOL;
-//                $items .= "<image:caption>{$item['caption']}</image:caption>" . PHP_EOL;
+                if (!empty($item['caption']))
+                    $items .= "<image:caption>{$item['caption']}</image:caption>" . PHP_EOL;
                 $items .= "<image:title>{$item['title']}</image:title>" . PHP_EOL;
                 $items .= "</image:image>" . PHP_EOL;
             }
--- sdk/Services/Seo/SiteMaps/ProductsSiteMap.php
+++ sdk/Services/Seo/SiteMaps/ProductsSiteMap.php
@@ -19,9 +19,11 @@ class ProductsSiteMap extends SiteMap
             $item['meta'] = json_decode($item['meta'] ?? '[]', true);
             $item['changefreq'] = $item['changefreq'] ?? 'hourly';
             $item['priority'] = $item['priority'] ?? '0.8';
-            $item['caption'] = html_entity_decode(sub_words($item['title'], 165), ENT_COMPAT, 'UTF-8');
-            $item['loc'] = route('lms.course', ['slug' => $item['slug']]);
+            $item['caption'] = html_entity_decode(sub_words($item['description'], 165), ENT_COMPAT, 'UTF-8');
+            $item['loc'] = site_url('course' . $item['slug']);
             $item['lastmod'] = date('Y-m-d\TH:i:s+03:30', strtotime($item['updated_at']));
+
+            $item['caption'] = strip_tags($item['caption']);
 
             $items .= '<url>' . PHP_EOL;
             $items .= "<loc>{$item['loc']}</loc>" . PHP_EOL;
@@ -32,7 +34,8 @@ class ProductsSiteMap extends SiteMap
             if (!empty($item['meta']['banner_url']['url'])) {
                 $items .= "<image:image>" . PHP_EOL;
                 $items .= "<image:loc>{$item['meta']['banner_url']['url']}</image:loc>" . PHP_EOL;
-                $items .= "<image:caption>{$item['caption']}</image:caption>" . PHP_EOL;
+                if (!empty($item['caption']))
+                    $items .= "<image:caption>{$item['caption']}</image:caption>" . PHP_EOL;
                 $items .= "<image:title>{$item['title']}</image:title>" . PHP_EOL;
                 $items .= "</image:image>" . PHP_EOL;
             }
