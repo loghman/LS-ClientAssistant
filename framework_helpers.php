ff5a9c3373b4d9b69a7c8adaf86a8d4d24eac233
Mohammad Gazori <amohammadgazori38@gmail.com>
2023-11-18 13:59:26 +0330

    update null

--- framework_helpers.php
+++ framework_helpers.php
@@ -1084,6 +1084,8 @@ if (!function_exists('telegram_simple_message')) {
 if (!function_exists('is_static_file')) {
     function is_static_file(string $url): bool
     {
+        if (empty($url)) return false;
+
         $staticExtensions = [
             'jpg',
             'jpeg',
