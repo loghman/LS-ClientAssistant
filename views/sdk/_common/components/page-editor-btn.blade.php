3c13be5c9a9ac972304bb6086fdc72dc44d60380
Ali khosravi <alikhosravi175@gmail.com>
2023-11-27 10:49:21 +0330

    Added Vite helper and moved page editor components to sdk

---
+++ views/sdk/_common/components/page-editor-btn.blade.php
@@ -0,0 +1,5 @@
+@if($canEdit and !$editMode)
+    <a href="?mode=edit" class="btn secondary btn-edit-mode px-3">
+        <i class="si-edit-r"></i>
+    </a>
+@endif
---
+++ views/sdk/_common/components/page-editor.blade.php
@@ -0,0 +1,13 @@
+@if(isset($canEdit) and isset($editMode))
+    <meta name="updateFormUrl" content="{{ route('pageEditor.store') }}">
+    <meta name="route_name" content="{{ $routeName ?? '' }}">
+    <meta name="entity_type" content="{{ $entityType ?? '' }}">
+    <meta name="entity_id" content="{{ $entityId ?? '' }}">
+
+    <meta name="uploader_base_url" content="{{ base_storage_url() }}">
+    <meta name="storage_jwt_token" content="{{ generate_storage_jwt_token(current_user_id()) }}">
+    <meta name="attachment_storage_url" content="{{ core_url('api/v1/core/file-manager/storeCKEditorFiles') }}">
+    <meta name="user_token" content="{{ current_user_token() }}">
+    <script src="{{ core_asset('resources/assets/js/clients-pageEditor.js') }}" type="module"></script>
+    <div class="modal page-editor-modal" id="modal-editor"></div>
+@endif
