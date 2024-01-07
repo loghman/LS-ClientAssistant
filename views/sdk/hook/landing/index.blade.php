51a75d2219b3103c5884af397c1008fe7560bb9d
miladboshagh <miladboshaghh@gmail.com>
2024-01-07 15:22:39 +0330

    test form components

--- views/sdk/hook/landing/index.blade.php
+++ views/sdk/hook/landing/index.blade.php
@@ -3,4 +3,10 @@
     سلام میلاد
     <h1>خوبی ؟؟</h1>
     <button>ارسال و دریافت چیت شیت پایتون</button>
+
+    <input type="text" placeholder="نام و نام خانوادگی را وارد کنید ...">
+    <div class="input-group">
+        <label>نام و نام خانوادگی</label>
+        <input type="text" placeholder="نام خود را وارد کنید ...">
+    </div>
 @endsection
