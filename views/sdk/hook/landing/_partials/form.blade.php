effb7e66c16311f1597307398fbf1da648de9f6e
miladboshagh <miladboshaghh@gmail.com>
2024-01-08 10:15:58 +0330

    add container

--- views/sdk/hook/landing/_partials/form.blade.php
+++ views/sdk/hook/landing/_partials/form.blade.php
@@ -1,26 +1,34 @@
-<div class="shape-image">
-    <img src="logo" alt="">
-</div>
-<h3 class="t-h3">
-    برای دریافت <span class="text-primary">چیت شیت پایتون</span>
-    فرم زیر رو پر کن
-</h3>
-<div class="shape-side top-horizontal">
-    <div class="card">
-        <div class="input-group">
-            <label>نام و نام خانوادگی</label>
-            <input type="text" placeholder="نام خود را وارد کنید ...">
+<div class="container-fluid">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="shape-image">
+                    <img src="logo" alt="">
+                </div>
+                <h3 class="t-h3">
+                    برای دریافت <span class="text-primary">چیت شیت پایتون</span>
+                    فرم زیر رو پر کن
+                </h3>
+                <div class="shape-side top-horizontal">
+                    <div class="card">
+                        <div class="input-group">
+                            <label>نام و نام خانوادگی</label>
+                            <input type="text" placeholder="نام خود را وارد کنید ...">
+                        </div>
+                        <div class="input-group">
+                            <label>شماره موبایل</label>
+                            <input type="text" placeholder="شماره موبایل را وارد کنید ...">
+                        </div>
+                        <div class="input-group">
+                            <label>ایمیل</label>
+                            <input type="text" placeholder="ایمیل خود را وارد کنید ...">
+                        </div>
+                        <button type="submit">ارسال و دریافت چیت شیت پایتون</button>
+                    </div>
+                </div>
+
+                <span class="t-title-sm">پس از اسال فرم، <span class="text-primary">چیت شیت</span> به ایمیل شما ارسال خواهد شد</span>
+            </div>
         </div>
-        <div class="input-group">
-            <label>شماره موبایل</label>
-            <input type="text" placeholder="شماره موبایل را وارد کنید ...">
-        </div>
-        <div class="input-group">
-            <label>ایمیل</label>
-            <input type="text" placeholder="ایمیل خود را وارد کنید ...">
-        </div>
-        <button type="submit">ارسال و دریافت چیت شیت پایتون</button>
     </div>
 </div>
-
-<span class="t-title-sm">پس از اسال فرم، <span class="text-primary">چیت شیت</span> به ایمیل شما ارسال خواهد شد</span>
