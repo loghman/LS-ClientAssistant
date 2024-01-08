929b0673a35f7bd9dad24ef537c0fe2eba4beae7
miladboshagh <miladboshaghh@gmail.com>
2024-01-08 12:12:03 +0330

    add subclass to components

--- views/sdk/hook/landing/_partials/header.blade.php
+++ views/sdk/hook/landing/_partials/header.blade.php
@@ -1,13 +1,13 @@
 <header class="ls-client-hook-header">
-    <span class="t-title">سون لرن</span>
-    <h1 class="t-h1">کتاب اصل گرایی</h1>
-    <div class="shape-side left-vertical">
+    <span class="ls-client-hook-t-title">سون لرن</span>
+    <h1 class="ls-client-hook-t-h1">کتاب اصل گرایی</h1>
+    <div class="ls-client-hook-shape-side left-vertical">
         <img src="banner" alt="">
         <div class="shape-image">
             <img src="logo" alt="">
         </div>
     </div>
-    <div class="tabs">
+    <div class="ls-client-hook-tabs">
         <a href="#" class="active">توضیحات</a>
         <a href="#">دریافت چیت شیت</a>
     </div>
