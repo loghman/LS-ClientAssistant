d43b9ccaddff41c2a8ba1b80156e46fa2dae90e6
miladboshagh <miladboshaghh@gmail.com>
2024-01-10 05:37:25 +0330

    last change

--- views/sdk/hook/landing/_partials/form.blade.php
+++ views/sdk/hook/landing/_partials/form.blade.php
@@ -1,8 +1,9 @@
 <div class="container-fluid ls-client-hook-section-footer">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-5">
+            <div class="col-xl-5 col-lg-7 col-10">
                 <div class="ls-client-hook-section-form">
+                    {!! $shapeArrow !!}
                     <div class="ls-client-hook-shape-image">
                         {!! $shapeImage !!}
                         <img src="{{ core_asset('resources/assets/img/clients/hook/download.svg') }}" alt="سون‌لرن" class="icon">
@@ -12,24 +13,28 @@
                         <br>
                         فرم زیر رو پر کن
                     </h2>
-                    <div class="ls-client-hook-shape-side top-horizontal">
+                    <div class="ls-client-hook-shape-side top-horizontal ls-client-hook-w-100">
                         <div class="ls-client-hook-card">
-                            <div class="ls-client-hook-input-group">
+                            <div class="ls-client-hook-input-group sm-convert-to-input">
                                 <label>نام و نام خانوادگی</label>
                                 <input type="text" placeholder="نام خود را وارد کنید ...">
                             </div>
-                            <div class="ls-client-hook-input-group">
+                            <div class="ls-client-hook-input-group sm-convert-to-input">
                                 <label>شماره موبایل</label>
                                 <input type="text" placeholder="شماره موبایل را وارد کنید ...">
                             </div>
-                            <div class="ls-client-hook-input-group">
+                            <div class="ls-client-hook-input-group sm-convert-to-input">
                                 <label>ایمیل</label>
                                 <input type="text" placeholder="ایمیل خود را وارد کنید ...">
                             </div>
-                            <button class="ls-client-hook-btn magnet-bottom" type="submit">ارسال و دریافت چیت شیت پایتون</button>
+                            <button class="ls-client-hook-btn magnet-bottom ls-client-hook-mx-auto" type="submit">
+                                ارسال و دریافت <span class="ls-client-hook-d-flex ls-client-hook-d-none-md">چیت شیت پایتون</span>
+                                {!! $iconArrowLeft !!}
+                            </button>
                         </div>
                     </div>
                     <span class="ls-client-hook-t-title-sm ls-client-hook-text-center">پس از اسال فرم، <span class="ls-client-hook-text-primary">چیت شیت</span> به ایمیل شما ارسال خواهد شد</span>
+                    {!! $shapeFooterLine !!}
                 </div>
             </div>
         </div>
@@ -37,4 +42,5 @@
     <div class="ls-client-hook-shape-bg">
         {!! $shapeFooter !!}
     </div>
+    {!! $shapeFooterPattern !!}
 </div>
--- views/sdk/hook/landing/_partials/header.blade.php
+++ views/sdk/hook/landing/_partials/header.blade.php
@@ -5,11 +5,15 @@
                 <header class="ls-client-hook-section-header">
                     <span class="ls-client-hook-t-title ls-client-hook-text-center">سون لرن</span>
                     <h1 class="ls-client-hook-t-h1 ls-client-hook-text-center">کتاب اصل گرایی</h1>
-                    <div class="ls-client-hook-shape-side top-vertical">
-                        <img class="banner" src="{{ core_asset('resources/assets/img/demo/hook-landing-banner.png') }}" alt="">
-                        <div class="ls-client-hook-shape-image magnet-top shadow">
-                            {!! $shapeImage !!}
-                            <img src="{{ core_asset('resources/assets/img/logo-icon.svg') }}" alt="سون‌لرن" class="icon">
+                    <div class="row justify-content-center ls-client-hook-w-100">
+                        <div class="col-xxxl-4 col-xl-5 col-lg-7 col-md-9 col-11">
+                            <div class="ls-client-hook-shape-side top-vertical">
+                                <img class="banner" src="{{ core_asset('resources/assets/img/demo/hook-landing-banner.png') }}" alt="">
+                                <div class="ls-client-hook-shape-image magnet-top shadow">
+                                    {!! $shapeImage !!}
+                                    <img src="{{ core_asset('resources/assets/img/logo-icon.svg') }}" alt="سون‌لرن" class="icon">
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="ls-client-hook-tabs">
