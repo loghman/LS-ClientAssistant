538741472e6a2506fb63096202160fab2e52f9fc
miladboshagh <miladboshaghh@gmail.com>
2024-01-11 12:06:44 +0330

    login form

--- views/sdk/hook/landing/_partials/_partials/_login-form.blade.php
+++ views/sdk/hook/landing/_partials/_partials/_login-form.blade.php
@@ -3,6 +3,10 @@
         class="{{ $subClass }}shape-side top-horizontal {{ $subClass }}w-100 ajax-form"
 >
     <div class="{{ $subClass }}card">
+        <span class="{{ $subClass }}t-title">سلام میلاد عزیز</span>
+        <p class="{{ $subClass }}t-text">
+            دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">چیت شیت پایتون</span> روی دکمه زیر کلیک کنید
+        </p>
         <button class="{{ $subClass }}btn magnet-bottom {{ $subClass }}mx-auto" type="submit">
             لاگین کنید <span class="{{ $subClass }}d-flex {{ $subClass }}d-none-md"></span>
             {!! $iconArrowLeft !!}
