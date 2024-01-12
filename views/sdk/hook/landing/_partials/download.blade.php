f596bb194f7d787a81861d9651fcd6da945efdc6
Amir Salehi <iamamirsalehi@gmail.com>
2024-01-12 09:27:04 +0330

    dowload view updated

--- views/sdk/hook/landing/_partials/download.blade.php
+++ views/sdk/hook/landing/_partials/download.blade.php
@@ -2,20 +2,20 @@
     {!! $shapeArrow !!}
     <div class="{{ $subClass }}shape-image">
         {!! $shapeImage !!}
-        <img src="{{ core_asset('resources/assets/img/clients/hook/download.svg') }}" alt="سون‌لرن" class="icon">
+        <img src="{{ core_asset('resources/assets/img/clients/hook/download.svg') }}" alt="{{ $brandName }}" class="icon">
     </div>
     <h2 class="{{ $subClass }}form-title {{ $subClass }}t-h2 {{ $subClass }}text-center">
-        دریافت <span class="{{ $subClass }}text-primary">چیت شیت پایتون</span>
+        دریافت <span class="{{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span>
     </h2>
     <div class="{{ $subClass }}shape-side top-horizontal {{ $subClass }}w-100">
         <div class="{{ $subClass }}card">
-            <span class="{{ $subClass }}t-title">سلام میلاد عزیز</span>
+            <span class="{{ $subClass }}t-title">سلام {{ !empty($user) ? $user['full_name'] : 'دوست'}} عزیز</span>
             <p class="{{ $subClass }}t-text">
-                دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">چیت شیت پایتون</span> روی دکمه زیر کلیک کنید
+                دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> روی دکمه زیر کلیک کنید
             </p>
             <span class="{{ $subClass }}t-h2 {{ $subClass }}text-center {{ $subClass }}countdown"></span>
             <button class="{{ $subClass }}btn magnet-bottom {{ $subClass }}mx-auto" type="submit">
-                دانلود مجدد <span class="{{ $subClass }}d-flex {{ $subClass }}d-none-md">چیت شیت پایتون</span>
+                دانلود مجدد <span class="{{ $subClass }}d-flex {{ $subClass }}d-none-md">{{ $hook['title_fa'] }}</span>
                 {!! $iconArrowLeft !!}
             </button>
         </div>
