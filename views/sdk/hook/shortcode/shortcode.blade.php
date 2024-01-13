23b3c5b5202c0c5167d9298979d70db89edd05b0
miladboshagh <miladboshaghh@gmail.com>
2024-01-13 13:55:44 +0330

    add title

--- views/sdk/hook/shortcode/shortcode.blade.php
+++ views/sdk/hook/shortcode/shortcode.blade.php
@@ -5,7 +5,7 @@ $subClass = 'ls-client-hook-';
 <div class="{{ $subClass }}card">
     <div class="{{ $subClass }}header">
         <img src="{{ core_asset('resources/assets/img/demo/hook-landing-banner.png') }}" alt="" class="{{ $subClass }}thumbnail">
-        <span class="{{ $subClass }}title"></span>
+        <span class="{{ $subClass }}title">کتاب اصل گرایی</span>
     </div>
     <p class="{{ $subClass }}text">اصل‌گرایی چگونگی انجام کارهای بیشتر نیست، بلکه به معنای انجام کارهای درست است. به معنای انجام کارهای کمتر فقط محض کمتر بودنشان هم نیست. بدین معناست که برای فعالیت در بالاترین سطح اثربخشی‌مان تا می‌توانیم عاقلانه زمان و انرژی‌مان را فقط روی انجام کارهای ضروری سرمایه‌گذاری کنیم. اصل‌گرایی یعنی خلق سیستمی برای رسیدگی به کمد زندگی‌مان. اصل‌گرایی فرآیندی نیست که مثل مرتب کردن کمد، یک‌بار در سال، یک‌بار در ماه یا حتی یک‌بار در هفته زیر بارش برویم.</p>
     <h3 class="{{ $subClass }}hr-text">برای دریافت <span class="{{ $subClass }}opacity">چیت شیت پایتون</span> فرم زیر رو پر کن</h3>
