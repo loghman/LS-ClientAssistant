864f0060ea814469067bf2711a0b463f047551e4
miladboshagh <miladboshaghh@gmail.com>
2024-01-15 21:50:29 +0330

    add auto-download to shortcode

--- views/sdk/hook/shortcode/download.blade.php
+++ views/sdk/hook/shortcode/download.blade.php
@@ -1,7 +1,7 @@
 <span class="{{ $subClass }}t-title-sm {{ $subClass }}hr-line">
         <span>دریافت <span class="{{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span></span>
 </span>
-<div class="{{ $subClass }}card-body">
+<div class="{{ $subClass }}card-body auto-download" data-redirect="{{ $shortLink }}" data-time="{{ $redirectTime }}" data-countdown=".{{ $subClass }}countdown">
     <span class="{{ $subClass }}t-title">سلام {{ !empty($user) ? $user['full_name'] : 'دوست'}} عزیز</span>
     <p class="{{ $subClass }}t-text">
         دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> روی دکمه زیر کلیک کنید
