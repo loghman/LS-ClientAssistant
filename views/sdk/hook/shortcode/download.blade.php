619cd954032aa4449116565700bbdc61e47d31a7
miladboshagh <miladboshaghh@gmail.com>
2024-01-15 22:45:59 +0330

    add shortlink

--- views/sdk/hook/shortcode/download.blade.php
+++ views/sdk/hook/shortcode/download.blade.php
@@ -7,7 +7,7 @@
         دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> روی دکمه زیر کلیک کنید
     </p>
     <span class="{{ $subClass }}t-h2 {{ $subClass }}text-center {{ $subClass }}countdown"></span>
-    <button data-redirect="$shortLink" class="link-redirect {{ $subClass }}btn {{ $subClass }}mx-auto" type="submit">
+    <button data-redirect="{{ $shortLink }}" class="link-redirect {{ $subClass }}btn {{ $subClass }}mx-auto" type="submit">
         <span>دانلود مجدد <span class="{{ $subClass }}d-inline {{ $subClass }}d-none-md">{{ $hook['title_fa'] }}</span></span>
     </button>
 </div>
