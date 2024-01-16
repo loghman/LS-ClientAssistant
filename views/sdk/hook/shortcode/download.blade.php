fda2f47a7d358e42918b00d654ff47998166a95b
miladboshagh <miladboshaghh@gmail.com>
2024-01-16 19:00:07 +0330

    refactor js loaded

--- views/sdk/hook/shortcode/download.blade.php
+++ views/sdk/hook/shortcode/download.blade.php
@@ -6,7 +6,7 @@
     <p class="{{ $subClass }}t-text">
         دانلود به صورت خودکار انجام میشه <br> اگر دانلود انجام نشد، برای دریافت <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> روی دکمه زیر کلیک کنید
     </p>
-    <span class="{{ $subClass }}t-title {{ $subClass }}text-center {{ $subClass }}countdown"></span>
+    <span class="{{ $subClass }}t-title {{ $subClass }}text-center {{ $subClass }}countdown {{ $subClass }}my-md"></span>
     <button data-redirect="{{ $shortLink }}" class="link-redirect {{ $subClass }}btn {{ $subClass }}mx-auto" type="submit">
         <span>دانلود مجدد <span class="{{ $subClass }}d-inline {{ $subClass }}d-none-md">{{ $hook['title_fa'] }}</span></span>
     </button>
