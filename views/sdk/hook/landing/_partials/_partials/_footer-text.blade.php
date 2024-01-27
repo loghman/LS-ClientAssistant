0c6585ba2b1aca0d6520ec7baedb07ddb5be14d0
Amir Salehi <iamamirsalehi@gmail.com>
2024-01-27 16:29:57 +0330

    footer and form button text updated

--- views/sdk/hook/landing/_partials/_partials/_footer-text.blade.php
+++ views/sdk/hook/landing/_partials/_partials/_footer-text.blade.php
@@ -1,7 +1,5 @@
 @if($hook['fields']['conditions']['hook_download_type'] == 'sendable')
-    <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-center">پس از اسال فرم، <span class="{{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> برای شما {{ ($hook['fields']['inputs']['email']['active'] && !$hook['fields']['inputs']['mobile']['active']) ? 'ایمیل' : 'پیامک' }}
-                        <br>
-                         خواهد شد</span>
+    <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-center">پس از تکمیل اطلاعات لینک دانلود فایل برای شما ارسال خواهد شد</span>
 @else
-    <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-center">پس از اسال فرم، <span class="{{ $subClass }}text-primary">{{ $hook['title_fa'] }}</span> را می‌توانید دانلود کنید</span>
+    <span class="{{ $subClass }}t-title-sm {{ $subClass }}text-center">پس از تکمیل اطلاعات میتوانید فایل را دانلود کنید</span>
 @endif
