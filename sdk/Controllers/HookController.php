6ebcaf362394899745d27d91ce039dc1c85a6305
Amir Salehi <iamamirsalehi@gmail.com>
2024-01-28 16:35:32 +0330

    sent-file-box renamed to sendable and download renamed to showable

--- sdk/Controllers/HookController.php
+++ sdk/Controllers/HookController.php
@@ -70,23 +70,20 @@ class HookController
             return JsonResponse::badRequest($response['message']['text']);
         }
 
-        $shortLink = $response['result']['short_link'];
+        $shortLink = $response['result']['short_link'] ?? '';
         $redirectTime = (int)setting('hook_showable_redirection_time');
         $subClass = 'ls-client-hook-';
         $hookDownloadType = $hook['fields']['conditions']['hook_download_type'];
-        if ($hookDownloadType == 'sendable') {
-            $message = $hook['fields']['inputs']['mobile']['active']
-                ? 'با تشکر، لینک دانلود ظرف ۵ دقیقه آینده برای شما پیامک خواهد شد'
-                : 'لینک دانلود فایل برای شما ارسال شد';
-            ;
-            return JsonResponse::ajaxView('sdk.hook.landing._partials.sent-file-box', compact('hook', 'subClass', 'user', 'message'));
-        }
+        $message = $hook['fields']['inputs']['mobile']['active']
+            ? 'با تشکر، لینک دانلود ظرف ۵ دقیقه آینده برای شما پیامک خواهد شد'
+            : 'لینک دانلود فایل برای شما ارسال شد';
+        ;
 
         if($request->get('from') == 'shortcode'){
             return JsonResponse::ajaxView('sdk.hook.shortcode.download', compact('shortLink', 'redirectTime', 'hook', 'subClass', 'user'));
         }
 
-        return JsonResponse::ajaxView('sdk.hook.landing._partials.download', compact('shortLink', 'redirectTime', 'hook', 'subClass', 'user'));
+        return JsonResponse::ajaxView(sprintf("sdk.hook.landing._partials.%s", $hookDownloadType), compact('hook', 'subClass', 'user', 'message','shortLink', 'redirectTime',));
     }
 
     public function signal(Request $request, $slug)
