48e9307aad28a39df5113ef2cb05bcb5006d4730
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-02-21 19:51:38 +0330

    landing form is transred to top of page

---
+++ views/sdk/hook/landing/_partials/banner.blade.php
@@ -0,0 +1,8 @@
+<h1 class="{{ $subClass }}t-h1 {{ $subClass }}text-center">{{ $hook['title_fa'] }}</h1>
+<div class="row justify-content-center {{ $subClass }}w-100">
+    <div class="col-xl-5 col-lg-7 col-md-9 col-11">
+        <div class="{{ $subClass }}shape-side top-vertical">
+            <img class="banner" src="{{ $hook['banner']['main']['url'] }}" alt="">
+        </div>
+    </div>
+</div>
