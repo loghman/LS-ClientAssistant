20b09ca5ad356c16f6c0671d1dd23a9bbc3edc9c
Amir Salehi <iamamirsalehi@gmail.com>
2024-02-26 10:47:24 +0330

    attachment duration is read from v3 standards

--- sdk/Controllers/MiniLandingController.php
+++ sdk/Controllers/MiniLandingController.php
@@ -19,9 +19,9 @@ class MiniLandingController
         $currentUser = current_user();
         $introVideo = $product['meta']['intro_video']['url'] ?? '';
         $maxDurationHours = Config::get('lms.max-duration-hours-display');
-        $productDuration = convert_seconds_to_hour($product['meta']['attachment_duration_sum']) > $maxDurationHours['en']
+        $productDuration = $product['attachment_duration_sum']['hours'] > $maxDurationHours['en']
             ? sprintf("بیش از %s ساعت", $maxDurationHours['fa'])
-            : to_persian_num(convert_seconds_to_hour($product['meta']['attachment_duration_sum'])) . '  ساعت آموزش';
+            : to_persian_num($product['attachment_duration_sum']['hours']) . '  ساعت آموزش';
 
         $hasCampaign = !empty($product['campaign_data']);
 
