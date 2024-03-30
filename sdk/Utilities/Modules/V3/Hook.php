8d84b3cba8d1272e113108d226c39e20b123b143
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-03-30 15:25:53 +0330

    hook sitemap is added

---
+++ sdk/Utilities/Modules/V3/Hook.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ls\ClientAssistant\Utilities\Modules\V3;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Collection;
+use Ls\ClientAssistant\Core\API;
+use Ls\ClientAssistant\Helpers\Response;
+
+class Hook
+{
+    public static function get(string $slug): Collection
+    {
+        try {
+            return API::get('client/v3/hook/' . $slug);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+
+    public static function sitemap(): Collection
+    {
+        try {
+            return API::get('client/v3/cms/hook/fetch/sitemap');
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+}
