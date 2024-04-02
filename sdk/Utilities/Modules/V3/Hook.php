dc397b8da753b30094adfbbfabbe284ab8010937
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-04-02 14:20:02 +0330

    hooke sitemap and seometa is added

---
+++ sdk/Utilities/Modules/V3/Hook.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ls\ClientAssistant\Utilities\Modules\V3;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Collection;
+use Ls\ClientAssistant\Core\API;
+use Ls\ClientAssistant\Helpers\Response;
+
+class Hook
+{
+    public static function list(ModuleFilter $filter = null): Collection
+    {
+        try {
+            return API::get('client/v3/cms/hook', $filter ? $filter->all() : []);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+
+    public static function get(string $slug): Collection
+    {
+        try {
+            return API::get('client/v3/hook/' . $slug);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+}
