c56deca9d176c4ea09d4e4e37ea19a5c8e119e2b
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-04-13 17:31:12 +0330

    fix some bugs on mini landing

--- views/sdk/pages/landing-partials/video.blade.php
+++ views/sdk/pages/landing-partials/video.blade.php
@@ -3,12 +3,16 @@
         <video controls>
             <source src="{{ $introVideo }}" type="video/mp4"/>
         </video>
-        <div class="cover">
-            <img src="{{ $product['banner_url']['main']['url'] }}" alt="{{ $product['title'] }}">
-            <i class="i-target"></i>
-            {{ $product['title'] }}
-        </div>
+        @if(isset($product['banner_url']['main']['url']))
+            <div class="cover">
+                <img src="{{ $product['banner_url']['main']['url'] }}" alt="{{ $product['title'] }}">
+                <i class="i-target"></i>
+                {{ $product['title'] }}
+            </div>
+        @endif
     </div>
 @else
-    <img src="{{ $product['banner_url']['main']['url'] }}" alt="{{ $product['title'] }}" class="thumbnail">
+    @if(isset($product['banner_url']['main']['url']))
+        <img src="{{ $product['banner_url']['main']['url'] }}" alt="{{ $product['title'] }}" class="thumbnail">
+    @endif
 @endif
