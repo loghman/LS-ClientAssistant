aec6ba7a9861b9ad0f294552f5491914bfd9b0f2
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-04-30 19:26:03 +0330

    add displayable hook

--- views/sdk/hook/landing/_partials/form.blade.php
+++ views/sdk/hook/landing/_partials/form.blade.php
@@ -4,32 +4,35 @@
             <div class="col-xxxl-5 col-xl-6 col-lg-7 col-md-10 col-11">
                 <div class="{{ $subClass }}section-form" id="section-form">
                     {!! $shapeArrow !!}
-                    <button onclick="showModal('https://karboom.io/jobs/rlgmbj/wordpress-php-developer-')" class="{{ $subClass }}btn">
-                        نمایش
-                     <i class="si-chevron-left-r cta"></i>
-                 </button>
-                 <div class="modal full" id="recruitment-modal">
-                    <div class="card p-0 position-relative">
-                        <div class="close-btn-absolute close">
+                    @if($hook['fields']['conditions']['hook_download_type'] == "displayable")
+                        <button onclick="showModal('{{$hook['download_short_url']}}')" class="{{ $subClass }}btn">
+                            نمایش
+                            <i class="si-chevron-left-r cta"></i>
+                        </button>
+                        <div class="modal full" id="recruitment-modal">
+                            <div class="card p-0 position-relative">
+                                <div class="close-btn-absolute close">
+                                </div>
+                                <iframe id="recruitment-iframe" width="100%" height="600px"></iframe>
+                            </div>
                         </div>
-                        <iframe id="recruitment-iframe" width="100%" height="600px"></iframe>
-                    </div>
-                </div>
-                    {{-- <div class="{{ $subClass }}shape-image">
-                        {!! $shapeImage !!}
-                        <img src="{{ core_asset('resources/assets/img/clients/hook/download.svg') }}"
-                            alt="{{ $brandName }}" class="icon">
-                    </div>
-                    <h2 class="{{ $subClass }}form-title {{ $subClass }}t-h4 {{ $subClass }}text-center">
-                        برای دانلود، اطلاعات زیر را وارد کنید. </h2>
-                    </h2>
-                    @if ($showLoginForm)
-                        @include('sdk.hook.landing._partials._partials._login-form')
                     @else
-                        @include('sdk.hook.landing._partials._partials._form')
+                        <div class="{{ $subClass }}shape-image">
+                            {!! $shapeImage !!}
+                            <img src="{{ core_asset('resources/assets/img/clients/hook/download.svg') }}"
+                                 alt="{{ $brandName }}" class="icon">
+                        </div>
+                        <h2 class="{{ $subClass }}form-title {{ $subClass }}t-h4 {{ $subClass }}text-center">
+                            برای دانلود، اطلاعات زیر را وارد کنید. </h2>
+                        </h2>
+                        @if ($showLoginForm)
+                            @include('sdk.hook.landing._partials._partials._login-form')
+                        @else
+                            @include('sdk.hook.landing._partials._partials._form')
+                        @endif
+                        @include('sdk.hook.landing._partials._partials._footer-text')
+                        {!! $shapeFooterLine !!}
                     @endif
-                    @include('sdk.hook.landing._partials._partials._footer-text') --}}
-                    {!! $shapeFooterLine !!}
                 </div>
             </div>
         </div>
@@ -39,11 +42,3 @@
     </div>
     {!! $shapeFooterPattern !!}
 </div>
-{{-- @section('modals')
-    <div class="modal full" id="recruitment-modal">
-        <div class="card p-0 position-relative">
-            <div class="close-btn-absolute close"></div>
-            <iframe id="recruitment-iframe" width="100%" height="600px"></iframe>
-        </div>
-    </div>
-@endsection --}}
