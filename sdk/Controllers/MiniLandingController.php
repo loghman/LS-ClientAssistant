bd828d21ae9f8e827cc0835f27331ebb26287772
Hamid Yeganehdoost <hamid1ganeh@gmail.com>
2024-05-07 10:24:24 +0330

    duration format is changed

--- sdk/Controllers/MiniLandingController.php
+++ sdk/Controllers/MiniLandingController.php
@@ -4,6 +4,7 @@ namespace Ls\ClientAssistant\Controllers;
 
 use Ls\ClientAssistant\Core\Router\JsonResponse;
 use Ls\ClientAssistant\Core\Router\WebResponse;
+use Ls\ClientAssistant\Helpers\Config;
 use Ls\ClientAssistant\Utilities\Modules\V3\Gateway;
 use Ls\ClientAssistant\Utilities\Modules\V3\LMSProduct;
 use Ls\ClientAssistant\Utilities\Modules\V3\ModuleFilter;
@@ -21,8 +22,15 @@ class MiniLandingController
         $brandNameEn = setting('brand_name_en');
         $currentUser = current_user();
         $introVideo = $product['meta']['intro_video']['url'] ?? ($product['meta']['demo_video_urls'][0] ?? '');
-        $productDuration = $product['meta']['attachment_duration_sum']['hours'] == 0 ? 0
-            : product_duration_to_string($product['meta']['attachment_duration_sum']['hours']);
+
+        $productDuration=0;
+        if ($product['meta']['attachment_duration_sum']['hours']>0){
+            $hours = $product['meta']['attachment_duration_sum']['hours'];
+            $maxDurationHours = Config::get('lms.max_duration_hours_display');
+            $productDuration =  $hours > $maxDurationHours['en']
+                ? sprintf("بیش از %s ساعت", $maxDurationHours['fa'])
+                : to_persian_num($hours);
+        }
 
         return WebResponse::view(
             'sdk.pages.mini-landing',
