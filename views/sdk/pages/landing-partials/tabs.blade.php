a0c4c2f4ec96a083a7440b44837ea5ac27de1d56
mohammad <mohammadghorbani1992@gmail.com>
2024-05-19 12:34:39 +0330

    in minimal landing dont show description tab if null and expand chapter if count is 1

--- views/sdk/pages/landing-partials/tabs.blade.php
+++ views/sdk/pages/landing-partials/tabs.blade.php
@@ -3,10 +3,12 @@
         <i class="i-check-lists-square-2"></i>
         <span>سرفصل</span>
     </li>
-    <li data-target="#tab-description">
-        <i class="i-align-right-2"></i>
-        <span>توضیحات</span>
-    </li>
+    @if($product['description'])
+        <li data-target="#tab-description">
+            <i class="i-align-right-2"></i>
+            <span>توضیحات</span>
+        </li>
+    @endif
     @if(count($product['productGifts']) > 0)
         <li data-target="#tab-gifts">
             <i class="i-gift"></i>
