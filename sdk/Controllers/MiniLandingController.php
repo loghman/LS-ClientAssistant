5788ff67ed95ed75b6f297e0458da64007c662ed
mohammad <mohammadghorbani1992@gmail.com>
2024-05-25 13:08:28 +0330

    fix show duration product

--- sdk/Controllers/MiniLandingController.php
+++ sdk/Controllers/MiniLandingController.php
@@ -21,8 +21,10 @@ class MiniLandingController
         $brandNameEn = setting('brand_name_en');
         $currentUser = current_user();
         $introVideo = $product['meta']['intro_video']['url'] ?? ($product['meta']['demo_video_urls'][0] ?? '');
-        $productDuration = $product['meta']['attachment_duration_sum']['hours'] == 0 ? 0
-            : product_duration_to_string_summary($product['meta']['attachment_duration_sum']['hours']);
+        $productDuration = 0;
+        if (isset($product['attachment_duration_sum']['hours']) && $product['attachment_duration_sum']['hours'] !== 0) {
+            $productDuration = product_duration_to_string_summary($product['attachment_duration_sum']['hours']);
+        }
 
         return WebResponse::view(
             'sdk.pages.mini-landing',
