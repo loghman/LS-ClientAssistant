17ac2184acb1773f615bab2301effae5f700043b
Reza Ghasemzadeh <rezaghasemzadeh1@gmail.com>
2024-06-18 10:52:56 +0330

    remove additional tag-manager

--- views/sdk/pages/landing-partials/head.blade.php
+++ views/sdk/pages/landing-partials/head.blade.php
@@ -18,16 +18,6 @@
 <meta name="application-name" content="7Learn">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
-
-@sectionMissing('without-tag-manager')
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5FBTZBV');</script>
-    <!-- End Google Tag Manager -->
-@endif
 @stack('head')
 
 {{ setting('bottom_of_head_script') }}
