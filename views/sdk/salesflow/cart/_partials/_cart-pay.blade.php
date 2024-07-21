dcba6880c8ca770259e4b5a9dd1699987c35d6de
miladboshagh <miladboshaghh@gmail.com>
2024-07-21 05:45:09 +0330

    edit payment gateways ui

--- views/sdk/salesflow/cart/_partials/_cart-pay.blade.php
+++ views/sdk/salesflow/cart/_partials/_cart-pay.blade.php
@@ -2,19 +2,22 @@
 
     <div id="gateway-container" class="d-flex flex-column gap-xs mt-sm"
          data-url_pattern="{{ route('payment.requestLink', ['cart' => $cart['id'], 'gateway' => '#gateway_id#']) }}">
-        <span class="t-title-md pb-xxs">درگاه‌های پرداخت</span>
-        <div class="d-flex align-items-start flex-wrap gap-xs">
+        <span class="t-title-md pb-xxs">
+            پرداخت نقدی
+            <small class="t-small me-xxs">(با تمامی کارت‌های بانکی)</small>
+        </span>
+        <div class="d-flex flex-column gap-xs ps-xxl ps-0--xxl">
             @foreach($gateways as $gateway)
                 @php($isSnap = isset($snapPay) && $gateway['id'] === $snapPay['id'])
                 @if($isSnap && (empty($eligibleResponse['successful']) || $eligibleResponse['successful'] !== true))
                     @continue
                 @endif
 
-                <label class="btn white-glass sm btn-input">
-                    <input type="radio" name="gateway"
-                           {{ $defaultGateway['id'] === $gateway['id'] ? 'checked' : '' }} value="{{ $gateway['id'] }}">
-                    <img class="icon" src="{{ $gateway['thumbnail'] }}" alt="{{ $gateway['name_fa'] }}">
+                <label class="card flex-row w-100 p-xs cursor-pointer gap-xs bg-white-50">
+                    <img class="w-fit me-xxs" width="34" height="34" src="{{ $gateway['thumbnail'] }}" alt="{{ $gateway['name_fa'] }}">
                     <span>{{ $isSnap ? $eligibleResponse['response']['title_message'] : $gateway['name_fa'] }}</span>
+                    <input class="me-auto ms-xxs success lg" type="radio" name="gateway"
+                           {{ $defaultGateway['id'] === $gateway['id'] ? 'checked' : '' }} value="{{ $gateway['id'] }}">
                 </label>
             @endforeach
             @if(!empty($eligibleResponse) && isset($snapPay))
@@ -28,6 +31,14 @@
                     </ul>
                 </div>
             @endif
+                <label class="card flex-row w-100 p-xs cursor-pointer gap-xs bg-white-50">
+                    <img class="w-fit me-xxs" width="34" height="34" src="{{ core_asset('resources/assets/img/gateways/snappay.svg') }}" alt="">
+                    <span class="content align-items-start gap-0">
+                        <span>پرداخت قسطی با اسنپ پی</span>
+                        <span class="card-microtitle">۴ قسط بدون کارمزد، ماهانه ۸۴,۲۵۰ تومان</span>
+                    </span>
+                    <input class="me-auto ms-xxs success lg" type="radio" name="gateway" value="snapp">
+                </label>
         </div>
     </div>
 
