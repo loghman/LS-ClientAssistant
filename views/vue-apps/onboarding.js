77e7f4fd62a90aa61795ed312a5d131c6599868a
mehdi behvar <97731800+mehdibehvar@users.noreply.github.com>
2024-07-27 12:03:46 +0330

    add-vue-persian-datepicker-theme

--- views/vue-apps/onboarding.js
+++ views/vue-apps/onboarding.js
@@ -7,6 +7,7 @@ import './assets/js/utilities/form-inputs.js'
 import OnboardingPage from './views/OnboardingPage.vue';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
+import Vue3PersianDatetimePicker from 'vue3-persian-datetime-picker'
 
 const app = createApp(OnboardingPage);
 const pinia = createPinia();
@@ -14,6 +15,13 @@ app.use(pinia);
 app.use(PrimeVue, {
   theme: {
       preset: Aura
+  }
+});
+app.use(Vue3PersianDatetimePicker, {
+  name: 'DatePicker',
+  props: {
+   color: "var(--primary)",
+   border: 'none',
   }
 });
 app.mount('#app');
