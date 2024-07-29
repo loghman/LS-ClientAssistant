b168ede1a5fe4c7462d88b1ab64204f915bac56d
Loghman <avand.loghman@gmail.com>
2024-07-29 14:19:58 +0330

    Update CoursePlayerController.php

--- sdk/Controllers/CoursePlayerController.php
+++ sdk/Controllers/CoursePlayerController.php
@@ -5,6 +5,7 @@ namespace Ls\ClientAssistant\Controllers;
 use Illuminate\Http\Request;
 use Ls\ClientAssistant\Core\Router\WebResponse;
 use Ls\ClientAssistant\Services\ObjectCache;
+use Ls\ClientAssistant\Utilities\Modules\Enrollment;
 use Ls\ClientAssistant\Utilities\Modules\LMSProduct;
 
 class CoursePlayerController
