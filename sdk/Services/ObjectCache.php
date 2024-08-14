d078d800439700b6d329b63ab6663c8713874ed6
Loghman <avand.loghman@gmail.com>
2024-08-14 16:59:12 +0330

    Update ObjectCache.php

--- sdk/Services/ObjectCache.php
+++ sdk/Services/ObjectCache.php
@@ -4,30 +4,33 @@ namespace Ls\ClientAssistant\Services;
 
 class ObjectCache
 {
-    private static $base = '../cache/static/';
+    private static $cacheFolder = '../cache/static/';
     private static $validtime = 3600;
     
     public static function exists($key){
-        $filepath = self::$base . md5($key) . '.oCache';
+        $filepath = self::$cacheFolder . md5($key) . '.oCache';
         return file_exists($filepath) && (time() - filemtime($filepath) < self::$validtime);
     }
 
     public static function get($key){
-        $filepath = self::$base . md5($key) . '.oCache';
+        $filepath = self::$cacheFolder . md5($key) . '.oCache';
         if(self::exists($key))
             return unserialize(file_get_contents($filepath));
         return false;
     }
 
     public static function write($key,$object){
-        $filepath = self::$base . md5($key) . '.oCache';
+        if(!is_dir(self::$cacheFolder)){
+            mkdir(self::$cacheFolder);
+        }
+        $filepath = self::$cacheFolder . md5($key) . '.oCache';
         file_put_contents($filepath,serialize($object));
         return $object;
     }
 
     public static function flush()
     {
-        $cacheFolder = self::$base;
+        $cacheFolder = self::$cacheFolder;
         $files = glob($cacheFolder . "*");
         foreach ($files as $file) {         
             if (is_file($file)) {
