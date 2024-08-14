987dec52075a38aba848fb974a15e85e733cc8d3
Loghman <avand.loghman@gmail.com>
2024-08-14 17:19:39 +0330

    Update textarea.blade.php

--- views/sdk/form-builder/textarea.blade.php
+++ views/sdk/form-builder/textarea.blade.php
@@ -1,8 +1,10 @@
+<?php $r=rand(0,999999); ?>
+
 <div class="step w-100">
     @if(isset($label))
         <p>{{ $label }}</p>
     @endif
-    <textarea name="{{ $name }}" id="var<?=$var['id']?>" rows="2" style="height: auto" placeholder="{{ $placeholder ?? ''  }}"
+    <textarea name="{{ $name }}" id="var<?=$r?>" rows="2" style="height: auto" placeholder="{{ $placeholder ?? ''  }}"
             class="{{ $classes ?? '' }}">{{ $value ?? '' }}</textarea>
-    <button type="button" class="d-none m-submit next-step sm float-start" data-required="var<?=$var['id']?>"> ثبت و بعدی </button>
+    <button type="button" class="d-none m-submit next-step sm float-start" data-required="var<?=$r?>"> ثبت و بعدی </button>
 </div>
