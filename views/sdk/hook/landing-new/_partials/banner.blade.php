ce994818ae1c5cd99c9a6e591bf5ecbb70cbac5a
Seyyed Ahmad Mousavi <mousavi7731@gmail.com>
2024-09-15 00:09:20 +0330

    Media transformer added to hooks

--- views/sdk/hook/landing-new/_partials/banner.blade.php
+++ views/sdk/hook/landing-new/_partials/banner.blade.php
@@ -2,7 +2,7 @@
 <div class="row justify-content-center {{ $subClass }}w-100">
     <div class="col-xl-5 col-lg-7 col-md-9 col-11">
         <div class="{{ $subClass }}shape-side top-vertical">
-            <img class="banner" src="{{ $hook['banner']['main']['url'] }}" alt="">
+            <img class="banner" src="{{ get_media_url($hook['banner'], get_default_media(\Ls\ClientAssistant\Utilities\Tools\Enums\MediaDefaultReplacementEnum::BANNER)) }}" alt="">
         </div>
     </div>
 </div>
