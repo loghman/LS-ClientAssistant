ed3f9928ec8bd6309b777a8e06ce7eaf6e84ba5c
mohammad <mohammadghorbani1992@gmail.com>
2024-10-05 17:52:37 +0330

    add LMSProductItem.php v3

---
+++ sdk/Utilities/Modules/V3/LMSProductItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ls\ClientAssistant\Utilities\Modules\V3;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Collection;
+use Ls\ClientAssistant\Core\API;
+use Ls\ClientAssistant\Helpers\Response;
+
+class LMSProductItem extends Cacher
+{
+    public static function list(ModuleFilter $filter = null): Collection
+    {
+        try {
+            return API::get('client/v3/lms/product-item', $filter ? $filter->all() : []);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+
+    public static function get(string $slug, ModuleFilter $filter = null): Collection
+    {
+        try {
+            return API::get('client/v3/lms/product-item/' . $slug, $filter ? $filter->all() : []);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+}
