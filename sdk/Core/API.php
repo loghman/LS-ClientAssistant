3cc65e6f118ca72236c1884194fcb1b7d04fda08
mohammad <mohammadghorbani1992@gmail.com>
2024-10-16 15:07:20 +0330

    refactore api class

--- sdk/Core/API.php
+++ sdk/Core/API.php
@@ -2,7 +2,6 @@
 
 namespace Ls\ClientAssistant\Core;
 
-use Composer\InstalledVersions;
 use Illuminate\Support\Collection;
 use Ls\ClientAssistant\Helpers\Config;
 use Ls\ClientAssistant\Utilities\Modules\User;
@@ -13,43 +12,30 @@ use Symfony\Component\HttpFoundation\Response;
 
 class API
 {
-    public static function get(string $uri, array $queryParam = [], array $headers = []): Collection
+    public static function get(string $uri, array $queryParam = [], $headers = []): Collection
     {
-        return self::sendRequest('GET', $uri, $queryParam, [], $headers);
+        $uri = $uri . '?' . http_build_query($queryParam);
+
+        $response = self::sendRequest('GET', $uri, [], $headers);
+
+        if ($response->get('status_code') === Response::HTTP_SERVICE_UNAVAILABLE) {
+            include_once dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'maintenance-mode.blade.php';
+            die;
+        }
+
+        if ($response->get('status_code') === Response::HTTP_MULTI_STATUS && isset($response['data']['redirect'])) {
+            header('Location: '.$response['data']['redirect'], true, $response['data']['code']);
+            exit;
+        }
+
+        return $response;
     }
 
-    public static function post(string $uri, array $formParams = [], array $headers = []): Collection
-    {
-        return self::sendRequest('POST', $uri, [], $formParams, $headers);
-    }
-
-    public static function put(string $uri, array $formParams = [], array $headers = []): Collection
-    {
-        return self::sendRequest('PUT', $uri, [], $formParams, $headers);
-    }
-
-    public static function patch(string $uri, array $formParams = [], array $headers = []): Collection
-    {
-        return self::sendRequest('PATCH', $uri, [], $formParams, $headers);
-    }
-
-    public static function delete(string $uri, array $formParams = [], array $headers = []): Collection
-    {
-        return self::sendRequest('DELETE', $uri, [], $formParams, $headers);
-    }
-
-    public static function getOrFromCache(
-        string $cacheKey,
-        array $config,
-        string $uri,
-        array $queryParam = [],
-        array $headers = []
-    ): Collection
+    public static function getOrFromCache(string $cacheKey, array $config, string $uri, array $queryParam = [], $headers = []): Collection
     {
         if (!$config['is_active']) {
             return self::get($uri, $queryParam, $headers);
         }
-
         $redisClient = Cache::getRedisInstance();
 
         if ($redisClient->exists($cacheKey)) {
@@ -65,106 +51,113 @@ class API
         return $data;
     }
 
-    private static function sendRequest(
-        string $method,
-        string $uri,
-        array  $queryParams = [],
-        array  $formParams = [],
-        array  $headers = []
-    ): Collection {
-        $headers = self::handleHeaders($headers);
-
-        $curl = curl_init();
-        $url = Config::get('endpoints.base').$uri;
-
-        if ($method === 'GET' && ! empty($queryParams)) {
-            $url .= '?'.http_build_query($queryParams);
-        }
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-        if (in_array($method, ['POST', 'PUT', 'PATCH', 'DELETE'])) {
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($formParams));
-        }
-
-        if (! empty($_ENV['IGNORE_SSL']) && ($_ENV['IGNORE_SSL'] === true || $_ENV['IGNORE_SSL'] === 'true')) {
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        }
-
-        $response = curl_exec($curl);
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-
-        return self::processResponse($response, $httpCode);
+    public static function put(string $uri, array $formParams = [], array $headers = []): Collection
+    {
+        return self::sendRequest('PUT', $uri, $formParams, $headers);
     }
 
-    private static function processResponse($response, $httpCode): Collection
+    public static function post(string $uri, array $formParams = [], array $headers = []): Collection
     {
-        $responseData = json_decode($response ?? '', true);
-        $responseData['status_code'] = $httpCode;
+        return self::sendRequest('POST', $uri, $formParams, $headers);
+    }
 
-        if ($httpCode === Response::HTTP_SERVICE_UNAVAILABLE) {
-            include_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'maintenance-mode.blade.php';
-            die;
+    public static function patch(string $uri, array $formParams = [], array $headers = []): Collection
+    {
+        return self::sendRequest('PATCH', $uri, $formParams, $headers);
+    }
+
+    public static function delete(string $uri, array $formParams = [], array $headers = []): Collection
+    {
+        return self::sendRequest('DELETE', $uri, $formParams, $headers);
+    }
+
+    public static function parseData($response, $statusCode): Collection
+    {
+        $data = json_decode($response ?? '', true);
+
+        $data['status_code'] = $statusCode;
+        if (isset($data['data']['data'])) {
+            return collect(Paginator::setLink($data));
+        } else if(is_array($data)) {
+            if(!isset($data['success'])) {
+                $data['success'] = $data['status'] ?? false;
+                unset($data['status']);
+            }
+            if(!isset($data['data'])) {
+                $data['data'] = $data['result'] ?? [];
+                unset($data['result']);
+            }
+            if(!isset($data['message']) && isset($data['errors'])) {
+                $data['message'] = is_array($data['errors']) ? $data['errors'][0] : ($data['errors'] ?? '');
+            }
+
+            return collect($data);
         }
 
-        if ($httpCode === Response::HTTP_FORBIDDEN && $responseData['message'] === 'You are not logged in.') {
-            self::manuallyLogout();
-        }
-
-        if ($httpCode === Response::HTTP_MULTI_STATUS && isset($responseData['data']['redirect'])) {
-            header('Location: '.$responseData['data']['redirect'], true, $responseData['data']['code']);
-            exit;
-        }
-
-        if (isset($responseData['data']['data'])) {
-            return collect(Paginator::setLink($responseData));
-        }
-
-        if (is_array($responseData)) {
-            $responseData['success'] = $responseData['success'] ?? $responseData['status'] ?? false;
-            $responseData['data'] = $responseData['data'] ?? $responseData['result'] ?? [];
-            $responseData['message'] = $responseData['message'] ?? ($responseData['errors'][0] ?? '');
-
-            return collect($responseData);
-        }
-
-        return collect(['success' => false, 'data' => [], 'message' => (string) $response]);
+        return collect([
+            'success' => false,
+            'data' => [],
+            'message' => (string) $response
+        ]);
     }
 
     private static function handleHeaders(array $headers): array
     {
         $ip = IP::get();
-        $cookies = self::mergeCookies();
-        $version = InstalledVersions::getVersion('ls/client-assistant');
 
-        $defaultHeaders = [
-            'Api-Key: '.$GLOBALS['apikey'],
+        foreach ($headers as $key => $header) {
+            if (str_contains(strtolower($header), 'authorization: bearer')) {
+                unset($headers[$key]);
+            }
+        }
+
+        $version = \Composer\InstalledVersions::getVersion('ls/client-assistant');
+        $cookies = self::mergeCookies();
+
+        $headerData = [
+            'Api-Key: ' . $GLOBALS['apikey'],
             'Content-Type: application/json',
-            'REAL-HTTP-CLIENT-IP: '.$ip,
-            'REAL-HTTP-CLIENT-AGENT: '.($_SERVER['HTTP_USER_AGENT'] ?? ''),
-            'REAL-HTTP-CLIENT-REFERRER: '.($_SERVER['HTTP_REFERER'] ?? ''),
-            'Authorization: Bearer '.User::getToken(),
+            'REAL-HTTP-CLIENT-IP: ' . $ip,
+            'REAL-HTTP-CLIENT-AGENT: ' . $_SERVER['HTTP_USER_AGENT'] ?? '',
+            'REAL-HTTP-CLIENT-REFERRER: ' . ($_SERVER['HTTP_REFERER'] ?? ''),
+            'Authorization: Bearer ' . User::getToken(),
             "Cookie: $cookies",
-            'LSPWEB-SDK-VERSION: '.$version,
+            'LSPWEB-SDK-VERSION: '. $version
         ];
 
-        return array_merge($defaultHeaders, $headers);
+        if (!empty($headers)) {
+            $headerData[] = 'JWT: ' . self::generateJwt($headers);
+        }
+
+        return array_merge($headerData, $headers);
     }
 
-    private static function mergeCookies(): string
+    public static function mergeCookies(): string
     {
-        return implode(
-            '; ',
-            array_map(
-                fn($key, $value) => "$key=$value",
-                array_keys($_COOKIE),
-                $_COOKIE
-            )
-        );
+        $str = '';
+        foreach ($_COOKIE as $key => $value) {
+            $str .="$key=$value;";
+        }
+        return $str;
+    }
+
+    private static function generateJwt(array &$headers): string
+    {
+        $key = $GLOBALS['apikey'];
+
+        $payload = [
+            'iss' => $GLOBALS['appUrl'],
+            'aud' => $GLOBALS['appUrl'],
+            'iat' => \Carbon\Carbon::now()->timestamp,
+            'exp' => \Carbon\Carbon::now()->addHour()->timestamp,
+            'ip' => $headers['ip'] ?? $_SERVER['REMOTE_ADDR'],
+            'token' => $headers['authorization'] ?? null,
+        ];
+
+        unset($headers['authorization']);
+        unset($headers['ip']);
+
+        return \Firebase\JWT\JWT::encode($payload, $key, 'HS256');
     }
 
     private static function manuallyLogout(): void
@@ -172,5 +165,39 @@ class API
         User::clearUserKeyCookie();
         Token::token()->remove();
         header("Refresh: 0");
+    }
+
+    private static function sendRequest(string $method, string $uri, array $body, array $headers)
+    {
+        if (!in_array($method, ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])) {
+            throw new \Exception('method is incorrect.');
+        }
+
+        $headers = self::handleHeaders($headers);
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, Config::get('endpoints.base') .$uri);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        if (!empty($headers)) {
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        }
+        if (!empty($body)) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
+        }
+        if (!empty($_ENV['IGNORE_SSL']) and $_ENV['IGNORE_SSL'] == true) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        $response = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+
+        if($httpCode === Response::HTTP_FORBIDDEN && $response->get('message') === 'You are not logged in.') {
+            self::manuallyLogout();
+            exit;
+        }
+
+        return self::parseData($response, $httpCode);
     }
 }
