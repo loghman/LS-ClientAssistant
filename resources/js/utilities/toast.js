b1d2a6a455ed415a43421ce2129d1d64bdb4326f
mehdi behvar <97731800+mehdibehvar@users.noreply.github.com>
2024-10-23 01:41:49 +0330

    change-position-and-time

--- resources/js/utilities/toast.js
+++ resources/js/utilities/toast.js
@@ -1,22 +1,22 @@
 export const toast = function (
     message,
-    type = 'success',
-    ttl = 7000,
-    position = 'top-left',
-    top= 40
-) {
+    type = "success",
+    ttl = 5000,
+    position,
+    top = 40
+  ) {
     // const topDistance=top?top:0
-    $('body').append(`<span style="top:${top}px" class="response-wrapper mt-1 toast-${position}  ${type} active">${message}</span>`);
-
-    setTimeout(
-        function () {
-            let wrapper = $('body > .response-wrapper');
-
-            wrapper.fadeOut();
-            wrapper.remove();
-        },
-        ttl
+    $("body").append(
+      `<span style="top:${top}px" class="response-wrapper mt-1 toast-top-right  ${type} active">${message}</span>`
     );
-};
-
-window.toast = toast;
+  
+    setTimeout(function () {
+      let wrapper = $("body > .response-wrapper");
+  
+      wrapper.fadeOut();
+      wrapper.remove();
+    }, ttl);
+  };
+  
+  window.toast = toast;
+  
