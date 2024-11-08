7bc5c21d88444058d3586f2fbff17c103aae3d2e
Loghman <avand.loghman@gmail.com>
2024-11-09 00:30:41 +0330

    Update common.blade.php

--- views/sdk/pages/common.blade.php
+++ views/sdk/pages/common.blade.php
@@ -1,11 +1,5 @@
 @extends('sdk._common.layouts.base')
-@if(isset($seoMeta) && $seoMeta != '')
-    @push('head')
-        {!! $seoMeta !!}
-    @endpush
-@else
-    @section('title', $article['title'])
-@endif
+@section('title', $page['title'])
 @push('head')
 @endpush
 @section('body-class', '')
@@ -14,16 +8,42 @@
 @endsection
 @section('base-content-class', '')
 @section('content')
-    <div class="container over-header my-4">
+<div class="container over-header my-4">
+        <h1><?= $page['title'] ?></h1>
         <div class="row gx-lg gx-md--lg">
             <div class="col-xxxl-12 col-xxl-8-5 col-xl-8">
-                <div class="row g">
+                <div class="row g page-content">
                     {!! $page['content'] !!}
                 </div>
             </div>
         </div>
     </div>
 @endsection
+<style>
+    header.square {
+        min-height: 300px !important;
+    }
+    h1{
+        margin: -190px 0 100px 0;
+        text-align: center;
+    }
+    @media (min-width:900px){
+        .page-content{
+            padding: 0 15%;
+        }
+    }
+    .page-content *{
+        line-height: 32px !important;
+    }
+    .page-content ul{
+        margin: 20px 50px 10px 10px !important;
+        display: block !important;
+    }
+    .page-content ul li{
+        list-style-type: circle;
+        display: list-item !important;
+    }
+</style>
 @section('modals')@endsection
 @section('footer')
     @include('_common.scripts.highlight')
