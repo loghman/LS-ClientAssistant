da1c65f7e8a2d760b76fb0c215078e43374ee4f7
mohammad <mohammadghorbani1992@gmail.com>
2025-02-25 11:09:21 +0330

    add coupon check to qpay

---
+++ sdk/Utilities/Modules/V3/Coupon.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ls\ClientAssistant\Utilities\Modules\V3;
+
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Support\Collection;
+use Ls\ClientAssistant\Core\API;
+use Ls\ClientAssistant\Helpers\Response;
+
+class Coupon extends Cacher
+{
+    public static function check(array $params): Collection
+    {
+        try {
+            return API::post('client/v3/salesflow/coupon/check', $params);
+        } catch (ClientException $exception) {
+            return Response::parseClientException($exception);
+        } catch (\Exception $exception) {
+            return Response::parseException($exception);
+        }
+    }
+}
