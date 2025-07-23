f662f3b78631d1f01c88967c9ef5c192da441390
mohammadrasoul-asghari <mohammadrasoulasghari1@gmail.com>
2025-07-23 14:20:17 +0330

    Optimize product feed retrieval by implementing object caching, reducing redundant API calls, and enhancing response handling.

--- sdk/Controllers/ProviderController.php
+++ sdk/Controllers/ProviderController.php
@@ -7,25 +7,33 @@ use Ls\ClientAssistant\Core\Router\JsonResponse;
 use Ls\ClientAssistant\Transformers\ProductFeedTransformer;
 use Ls\ClientAssistant\Utilities\Modules\V3\LMSProduct;
 use Ls\ClientAssistant\Utilities\Modules\V3\ModuleFilter;
+use Ls\ClientAssistant\Services\ObjectCache;
 
 class ProviderController
 {
     public function productFeed(Request $request)
     {
         try {
-            $filter = (new ModuleFilter())
-                ->includes("category")
-                ->page($request->page);
+            $key = 'ProductFeed_' . ($request->page ?? 1);
+            
+            if (ObjectCache::exists($key)) {
+                $transformedData = ObjectCache::get($key);
+            } else {
+                $filter = (new ModuleFilter())
+                    ->includes("category")
+                    ->page($request->page?? 1);
 
-            $response = LMSProduct::list($filter);
-
-            if (!$response->get('success', false)) {
-                return JsonResponse::unprocessableEntity('خطا در دریافت محصولات', []);
+                $response = LMSProduct::list($filter);
+                
+                if (!$response->get('success', false)) {
+                    return JsonResponse::unprocessableEntity('خطا در دریافت محصولات', []);
+                }
+                
+                $transformedData = ProductFeedTransformer::collection($response);
+                $transformedData = ObjectCache::write($key, $transformedData->toArray());
             }
             
-            $transformedData = ProductFeedTransformer::collection($response);
-            
-            return JsonResponse::success('لیست محصولات با موفقیت دریافت شد', $transformedData->toArray());
+            return JsonResponse::success('لیست محصولات با موفقیت دریافت شد', $transformedData);
 
         } catch (\Exception $e) {
             return JsonResponse::unprocessableEntity('خطا در پردازش درخواست', ['error' => $e->getMessage()]);
