f848cd970f8453afa0c805e4055b04999d2314e2
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2025-08-09 16:10:04 +0330

    handle paginate and create a service

--- sdk/Controllers/SiteMapController.php
+++ sdk/Controllers/SiteMapController.php
@@ -3,6 +3,7 @@
 namespace Ls\ClientAssistant\Controllers;
 
 use Ls\ClientAssistant\Core\Router\WebResponse;
+use Ls\ClientAssistant\Services\SitemapService;
 use Ls\ClientAssistant\Utilities\Modules\V3\Hook;
 use Ls\ClientAssistant\Utilities\Modules\V3\ModuleFilter;
 use Ls\ClientAssistant\Utilities\Modules\CMS;
@@ -11,29 +12,18 @@ use Ls\ClientAssistant\Utilities\Tools\Sitemap;
 
 class SiteMapController
 {
+
+    public function __construct(private SitemapService $sitemapService)
+    {
+    }
     public function sitemap()
     {
         Sitemap::cache('index');
-        
-        // Get total pages for posts sitemap
-        $totalPages = $this->getPostsTotalPages();
-        
         $siteMaps = [
             [
                 'loc' => site_url('sitemap-static.xml'),
                 'lastmod' => '2024-05-07T19:12:26+03:30'
-            ]
-        ];
-        
-        // Add paginated posts sitemaps
-        for ($page = 1; $page <= $totalPages; $page++) {
-            $siteMaps[] = [
-                'loc' => site_url("sitemap-posts-page{$page}.xml"),
-                'lastmod' => '2024-05-07T19:12:26+03:30'
-            ];
-        }
-        
-        $siteMaps = array_merge($siteMaps, [
+            ],
             [
                 'loc' => site_url('sitemap-pages.xml'),
                 'lastmod' => '2024-05-07T19:12:26+03:30'
@@ -46,26 +36,12 @@ class SiteMapController
                 'loc' => site_url('sitemap-hooks.xml'),
                 'lastmod' => '2024-05-07T19:12:26+03:30'
             ],
-        ]);
+        ];
+        
+        $paginatedPostsUrls = $this->sitemapService->generatePaginatedPostsSitemapUrls();
+        $siteMaps = array_merge($siteMaps, $paginatedPostsUrls);
         
         WebResponse::sitemap('index', $siteMaps);
-    }
-
-    private function getPostsTotalPages(): int
-    {
-        $filters = [
-            'type' => ['post', 'qa', 'video', 'podcast', 'terminology'],
-            'status' => 'published',
-        ];
-
-        // Get first page to get total count from pagination meta
-        $response = CMS::queryParams([
-            'filters' => $filters,
-            'page' => 1
-        ], [], [], 1);
-
-        $total = $response['data']['total'] ?? 0;
-        return (int) ceil($total / 100); // 100 posts per page
     }
 
     public function staticSiteMap()
@@ -76,40 +52,12 @@ class SiteMapController
     }
     public function postsSiteMap()
     {
-
-        return $this->postsSiteMapPaginated(1);
+        $this->sitemapService->generateDefaultPostsSitemap();
     }
 
     public function postsSiteMapPaginated(int $page = 1)
     {
-        Sitemap::cache("posts-page{$page}");
-        $filters = [
-            'type' => ['post', 'qa', 'video', 'podcast', 'terminology'],
-            'status' => 'published',
-            'order-by' => 'published_at',
-            'dir' => 'desc',
-        ];
-
-        $posts = CMS::queryParams([
-            'filters' => $filters,
-            'page' => $page
-        ], [
-            [
-                [
-                    'columns' => [
-                        'title',
-                        'slug',
-                        'thumbnail',
-                        'content',
-                        'updated_at',
-                        'published_at',
-                        'meta'
-                    ]
-                ]
-            ]
-        ], [], 100)['data']['data'];
-
-        WebResponse::sitemap('posts', $posts, "posts-page{$page}");
+        $this->sitemapService->generatePostsSitemap($page);
     }
 
     public function pagesSiteMap()
--- sdk/Utilities/Tools/Sitemap.php
+++ sdk/Utilities/Tools/Sitemap.php
@@ -7,7 +7,8 @@ class Sitemap {
     public static function cache(string $name)
     {
         $cache_file = dirname(__DIR__, 3) . DIRECTORY_SEPARATOR . 'cache'. DIRECTORY_SEPARATOR . 'sitemap' . DIRECTORY_SEPARATOR . $name . '.xml';
-        $cache_time = env('SITEMAP_CACHE_HOUR', 6) * 60 * 60;
+        $cache_hours = (int) env('SITEMAP_CACHE_HOUR', 6);
+        $cache_time = $cache_hours * 60 * 60;
         if (!file_exists($cache_file) or (time() - filemtime($cache_file)) > $cache_time) {
             return false;
         }
